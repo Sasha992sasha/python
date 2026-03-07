c6cff40b215bbcf7b0bdc138eaafae522250cf36
S9s <svidkosasa94@gmail.com>
2026-03-07 13:16:52 +0200

    ne dorobiv(

--- dz3/2.py
+++ dz3/2.py
@@ -3,3 +3,26 @@ class Product:
         self.name = name
         self.price = price
         self.count = count
+
+class Cart:
+    def __init__(self):
+        self.spisok = []
+
+    def add(self , prod):
+        self.spisok.append(prod)
+    
+    def remove(self,index):
+        self.spisok.pop(index)
+    
+    def printer(self):
+        pass
+
+t1 = Product("Яблуко" , 20 , 1)
+t2 = Product("Молоко" , 2 ,13)
+
+cart = Cart()
+
+cart.add (t1)
+cart.add (t2)
+
+cart.printer()
